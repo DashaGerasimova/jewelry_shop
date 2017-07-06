39d029916a3c3bb05f7ad9507d42e286f845aeb6
Dasha <dashagera@gmail.com>
2017-07-06 16:14:28 +0300

    Added styles, scripts to builder

--- app/models/jewel.rb
+++ app/models/jewel.rb
@@ -1,4 +1,4 @@
 class Jewel < ApplicationRecord
-  validates :name, :jewel_type, :size, :image, :price, presence: true
+  validates :name, :size, :price, presence: true
   validates :size, inclusion: { in: 17..30, message: "should be between 17 and 30" }
 end
