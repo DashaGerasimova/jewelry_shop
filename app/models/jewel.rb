653d3c494bc144306fac1d716984a478059d6b87
Dasha <dashagera@gmail.com>
2017-07-06 14:11:26 +0300

    Copypasted builder's template and images

--- app/models/jewel.rb
+++ app/models/jewel.rb
@@ -1,5 +1,4 @@
 class Jewel < ApplicationRecord
   validates :name, :jewel_type, :size, :image, :price, presence: true
   validates :size, inclusion: { in: 17..30, message: "should be between 17 and 30" }
-  validates_format_of :image, :with => %r{\.png\z}i, message: "Image should have .png format"
 end
