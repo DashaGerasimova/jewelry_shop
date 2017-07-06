832c3b17bf87b859346b070a18f35a93f909490b
Dasha <dashagera@gmail.com>
2017-07-06 11:46:34 +0300

    Installed devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :jewels
   
-  root 'jewels#index'
+  root to:'jewels#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170705083848) do
+ActiveRecord::Schema.define(version: 20170706083715) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "desc"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "jewel_id"
+    t.index ["jewel_id"], name: "index_comments_on_jewel_id"
+  end
 
   create_table "jewels", force: :cascade do |t|
     t.string "name"
@@ -21,6 +30,23 @@ ActiveRecord::Schema.define(version: 20170705083848) do
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
