8ac3686c075feb9a03b6bc014643be81fefa1dfc
Dasha <dashagera@gmail.com>
2017-07-10 18:36:34 +0300

    Added comment deletion and updating

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,17 @@
 class CommentsController < ApplicationController
   before_action :set_product
+  before_action :set_comment, only: [:edit, :update, :destroy]
 
-  # GET /comments/1
-  # GET /comments/1.json
   def show
   end
 
-  # GET /comments/new
   def new
     @comment = Comment.new
   end
 
-  # GET /comments/1/edit
   def edit
   end
 
-  # POST /comments
-  # POST /comments.json
   def create
     @comment = @product.comments.create(comment_params)
     @comment.user_id = current_user.id
@@ -25,38 +20,26 @@ class CommentsController < ApplicationController
       redirect_to @product, notice: 'Comment was successfully created.' 
     else
       flash.now[:alert] = "error"
-    end
-    
+    end    
   end
 
-  # PATCH/PUT /comments/1
-  # PATCH/PUT /comments/1.json
   def update
-    respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
-      else
-        format.html { render :edit }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
+    if @comment.update(comment_params)
+      redirect_to @product, notice: 'Comment was successfully updated.' 
+    else
+      flash.now[:alert] = "error"
     end
   end
 
-  # DELETE /comments/1
-  # DELETE /comments/1.json
   def destroy
     @comment.destroy
-    respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to @product, notice: 'Comment was successfully destroyed.' 
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_comment
-      @comment = Comment.find(params[:id])
+      @comment = @product.comments.find(params[:id])
     end
 
     def set_product
