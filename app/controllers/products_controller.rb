68978d0466796871bf2ae7734793098a93121c6e
Dasha <dashagera@gmail.com>
2017-07-11 12:51:58 +0300

    Added expose and respond_with to products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,11 @@
 require 'base64'
 
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:show, :edit, :update, :destroy]
+  expose :products, ->{ Product.all }
+  expose :product
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   
   def index
-    @products = Product.all
   end
 
   
@@ -13,41 +13,27 @@ class ProductsController < ApplicationController
   end
 
   def new
-    @product = Product.new
   end
 
   def edit
   end
 
   def create
-    @product = Product.new(product_params)
-
-    if @product.save
-      redirect_to @product, notice: 'product was successfully created.'
-    else
-      render :new 
-    end
+    product.save
+    respond_with product
   end
 
   def update
-    if @product.update(product_params)
-      redirect_to @product, notice: 'product was successfully updated.'
-    else
-      render :edit
-    end
+    product.update(product_params)
+    respond_with product
   end
 
   def destroy
-    @product.destroy
-    redirect_to products_url, notice: 'product was successfully destroyed.'
+    product.destroy
+    respond_with product
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_product
-      @product = Product.find(params[:id])
-    end
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
       params.require(:product).permit(:name, :desc, :style, :size, :image, :price)
