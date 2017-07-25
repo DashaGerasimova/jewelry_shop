17fa19798b22ad2423e57154ef2631edb364bbdc
Dasha <dashagera@gmail.com>
2017-07-25 16:19:23 +0300

    Added status column to orders table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170725130407) do
+ActiveRecord::Schema.define(version: 20170725131542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20170725130407) do
     t.integer "pay_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status"
   end
 
   create_table "products", force: :cascade do |t|
