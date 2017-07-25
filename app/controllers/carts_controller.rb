6cfede957bedb68a1c8f76bfb0a41157cae8bc32
Dasha <dashagera@gmail.com>
2017-07-25 13:46:23 +0300

    Improved cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,13 +1,8 @@
 class CartsController < ApplicationController
   include CurrentCart   #for set_cart method
-  expose(:product) { set_product }
+  expose(:product) { set_product(params[:product_id]) }
   expose(:cart) { set_cart }
-
-  def index
-  end
-
-  def new
-  end
+  expose(:sorted_cart) { sort_cart }
 
   def create
     cart << product.id
@@ -20,7 +15,11 @@ class CartsController < ApplicationController
   end
 
   private
-    def set_product
-      Product.find(params[:product_id])
+    def set_product(product_id)
+      Product.find(product_id)
     end 
+
+    def sort_cart
+      cart.inject(Hash.new(0)) { |quantity, product| quantity[product] += 1 ;quantity}
+    end
 end
