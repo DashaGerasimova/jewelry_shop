e46393ba57fb8900bd9cd1331192c9c80c12f404
Dasha <dashagera@gmail.com>
2017-07-28 12:37:22 +0300

    Changed pay_type and status columns types in orders table

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,16 +5,16 @@ class Order < ApplicationRecord
   after_create_commit :send_order_create_notification
   after_update_commit :send_order_status_update_notification
 
-  enum pay_type: {
-    "Cash" => 0, 
-    "Credit Card" => 1,
-    "Check" => 2
-  }
-  enum status: {
-    "Not staged" => 0, 
-    "In progress" => 1,
-    "Ready" => 2
-  }
+  pay_types = [
+    "Cash", 
+    "Credit Card",
+    "Check"
+  ]
+  statuses = [
+    "Not staged",
+    "In progress",
+    "Ready"
+  ]
 
   def send_order_create_notification
     OrderMailer.delay.order_create_notification(self)
---
+++ db/migrate/20170728093532_change_pay_type_in_orders.rb
@@ -0,0 +1,5 @@
+class ChangePayTypeInOrders < ActiveRecord::Migration[5.1]
+  def change
+    change_column :orders, :pay_type, :string
+  end
+end
