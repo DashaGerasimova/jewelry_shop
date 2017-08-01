b0a96c85d7c30beff314adcd1a48675171a8311e
Dasha <dashagera@gmail.com>
2017-08-01 17:01:34 +0300

    Temporarly removed uploading to amazon(because my aws account is broken)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -49,14 +49,14 @@ class ProductsController < ApplicationController
         f.write image_data
       end
 
-      service = Aws::S3::Resource.new(region: "us-east-2")
-      bucket_name = "jewelry-shop-images"
-      bucket = service.bucket(bucket_name)
+      # service = Aws::S3::Resource.new(region: "us-east-2")
+      # bucket_name = "jewelry-shop-images"
+      # bucket = service.bucket(bucket_name)
 
-      key = "images/" + File.basename(image_location)
-      s3_file = bucket.object(key)
-      s3_file.upload_file(image_location)
-      product.image = s3_file.public_url
+      # key = "images/" + File.basename(image_location)
+      # s3_file = bucket.object(key)
+      # s3_file.upload_file(image_location)
+      product.image = "/uploads/#{product.id}.png"
       product.save
     end    
     # Never trust parameters from the scary internet, only allow the white list through.
