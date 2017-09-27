981ed7272b59123c0ee0b1f4aa54025180dafa2d
Dasha <dashagera@gmail.com>
2017-09-27 17:14:25 +0300

    Fixed cart interactor

--- app/interactors/create_order_products_from_cart.rb
+++ app/interactors/create_order_products_from_cart.rb
@@ -2,11 +2,9 @@ class CreateOrderProductsFromCart
   include Interactor
 
   def call
-    def create_order_products_from_cart
-      context.cart.each do |product_id, quantity|
-        order_product = OrderProduct.new(product_id: product_id, order_id: context.order.id, cost: Product.find(product_id).price * quantity, quantity: quantity)
-        order_product.save
-      end
+    context.cart.each do |product_id, quantity|
+      order_product = OrderProduct.new(product_id: product_id, order_id: context.order.id, cost: Product.find(product_id).price * quantity, quantity: quantity)
+      order_product.save
     end
   end
 end
