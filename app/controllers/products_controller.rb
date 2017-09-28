8e58f252392fc0b94caef2b048d9db6b863c80d4
Dasha <dashagera@gmail.com>
2017-09-28 15:43:47 +0300

    Fixed products search

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,6 @@
-require 'base64'
-require 'aws-sdk'
-
 class ProductsController < ApplicationController
-  expose_decorated(:product)
-  expose(:products) { Product.all.decorate }
+  expose_decorated(:product) 
+  expose(:products) { find_sort_products }
   def create
     product.user = current_user
     
@@ -25,8 +22,8 @@ class ProductsController < ApplicationController
 
   private  
     # Never trust parameters from the scary internet, only allow the white list through.
-    def set_find_sort_products
-      Product.search(params[:search_term]).all.sort_by {|product| product.rating }.reverse
+    def find_sort_products
+      Product.search(params[:search_term]).all.decorate.sort_by {|product| product.rating }.reverse
     end
     def product_params
       params.require(:product).permit(:name, :desc, :style, :size, :image, :price)
